9e063f98332a42ca2d7da104f1e1d2a6105dcf84
sakram07 <MarkasCerniauskas@gmail.com>
2016-11-01 20:16:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 from bs4 import BeautifulSoup as bsoup
 from urllib.request import Request, urlopen
 
-baseLink = 'http://www.wuxiaworld.com/st-index/st-book-3-chapter-'
-
-req = Request(baseLink, headers={'User-Agent': 'Mozilla/5.0'})
-sauce = urlopen(req).read()
-
-soup = bsoup(sauce, 'lxml')
+baseLink = 'http://www.wuxiaworld.com/st-index/st-book-3-chapter-' #To change the book you're scrapping, change this line
 
 for i in range(1, 25):
     req = Request(baseLink + str(i), headers={'User-Agent': 'Mozilla/5.0'})
