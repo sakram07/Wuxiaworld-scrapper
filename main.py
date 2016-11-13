88f9d1f85e87b567932d4cd88dc16d6179d9cfb3
sakram07 <MarkasCerniauskas@gmail.com>
2016-11-13 20:56:23 +0200

    Now writes to a file
    Instead of printing to the console as it used to it now creates a text file called book.txt

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from bs4 import BeautifulSoup as bsoup
 from urllib.request import Request, urlopen
 
-baseLink = 'http://www.wuxiaworld.com/st-index/st-book-3-chapter-' #To change the book you're scrapping, change this line
+result = open("book.txt", "w")
 
-for i in range(1, 25):
+baseLink = 'http://www.wuxiaworld.com/st-index/st-book-5-chapter-' #To change the book you're scrapping, change this line
+
+for i in range(1, 35):
     req = Request(baseLink + str(i), headers={'User-Agent': 'Mozilla/5.0'})
     sauce = urlopen(req).read()
     soup = bsoup(sauce, 'lxml')
     for paragraph in soup.find("div", {"itemprop":"articleBody"}).find_all('p'):
-        print(paragraph.string)
+        try:
+            result.write(paragraph.string)
+            result.write('\n')
+        except:
+            print ("something might not have been printed, but it's likely not important")
